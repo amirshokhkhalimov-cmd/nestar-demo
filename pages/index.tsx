e187d42b9e51827d866c81fe95ad546995397afc
Amirshokh Khalimov <amirshokhkhalimov@gmail.com>
2026-05-03 20:03:33 +0900

    fix: integration of header and footer via layout

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import withLayoutMain from "@/libs/components/layout/LayoutHome";
+import { Box, Container, Stack } from "@mui/material";
+import { NextPage } from "next";
+
+const  Home:NextPage = ()=>  {
+  return (
+    
+      
+      <Container>
+        <Stack flexDirection={"column"}>
+          <Box>Popular Properties</Box>
+          <Box>Top Agents</Box>
+          <Box>Top Properties</Box>
+          <Box>Events</Box>
+        </Stack>
+      </Container>
+      
+  
+  );
+};
+export default withLayoutMain(Home);
---
+++ pages/property/index.tsx
@@ -0,0 +1,16 @@
+import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
+import { Container, Stack } from "@mui/material";
+import { NextPage } from "next";
+
+const PropertyList: NextPage = () => {
+    console.log("PROPERTYLIST COMPONENT - PAGES ROUTER");
+    return (
+
+
+        <Container>PROPERTY LIST</Container>
+
+
+    );
+};
+
+export default withLayoutBasic(PropertyList);
